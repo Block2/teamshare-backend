27f8dc61ebb7ac05bef8436596db5c99aafed7ca
xuwenfeng <xwf@winning.com.cn>
2019-06-06 13:59:06 +0800

    temp

--- src/main/java/winning/service/ArticleService.java
+++ src/main/java/winning/service/ArticleService.java
@@ -8,6 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.LinkedCaseInsensitiveMap;
 
 import javax.annotation.Resource;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 import java.util.Map;
 
@@ -24,65 +25,82 @@ public class ArticleService {
     @Transactional
     public Map getArticleByMcid(Map<String, String> paramMap) {
 
-        Map map=new LinkedCaseInsensitiveMap();
-        List<Map> list =  articleDao.getArticlesByMcid(paramMap.get("MCID"));
+        Map map = new LinkedCaseInsensitiveMap();
+        List<Map> list = articleDao.getArticlesByMcid(paramMap.get("MCID"));
         map.put("ARTICLE_LIST", list);
         return map;
     }
 
     @Transactional
-    public Map insertArticle(Map<String, String> paramMap){
+    public Map insertArticle(Map<String, String> paramMap) {
 
-        Map map=new LinkedCaseInsensitiveMap();
+        Map map = new LinkedCaseInsensitiveMap();
+        Article article = new Article();
+        int a = 0;
+        try {
+            if (!isUndefined(paramMap.get("TITLE"))) {
+                article.setTitle(paramMap.get("TITLE"));
+            }
+            if (!isUndefined(paramMap.get("AUTHOR"))) {
+                article.setAuthor(paramMap.get("AUTHOR"));
+            }
+            if (!isUndefined(paramMap.get("TAG"))) {
+                article.setTag(paramMap.get("TAG"));
+            }
+            if (!isUndefined(paramMap.get("BODYPART"))) {
+                article.setBodypart(paramMap.get("BODYPART").getBytes("utf8"));
+            }
+            if (!isUndefined(paramMap.get("CREATETIME"))) {
+                article.setCreateTime(paramMap.get("CREATETIME"));
+            }
 
-        Article article = JsonUtils.jsonStrToBean("ARTICLE", Article.class);
-        int a = articleDao.insertArticle(article);
+            a = articleDao.insertArticle(article);
+        } catch (UnsupportedEncodingException e) {
+            a = 0;
+        }
 
-        String msg = a > 0? "保存成功" : "保存失败";
+        String msg = a > 0 ? "保存成功" : "保存失败";
         map.put("IS_EXIST", msg);
         return map;
     }
 
     @Transactional
-    public Map updateArticle(Map<String, String> paramMap){
+    public Map updateArticle(Map<String, String> paramMap) {
 
-        Map map=new LinkedCaseInsensitiveMap();
+        Map map = new LinkedCaseInsensitiveMap();
 
         Article article = JsonUtils.jsonStrToBean("ARTICLE", Article.class);
         int a = articleDao.updateArticle(article);
 
-        String msg = a > 0? "修改成功" : "修改失败";
+        String msg = a > 0 ? "修改成功" : "修改失败";
         map.put("IS_EXIST", msg);
         return map;
     }
 
     @Transactional
-    public Map getArticleById(Map<String, String> paramMap){
+    public Map getArticleById(Map<String, String> paramMap) {
+
+        if (paramMap.get("AID").startsWith("unDefined")) {
+            return null;
+        }
         return articleDao.getArticleById(paramMap.get("AID"));
     }
 
     @Transactional
-    public Map deleteArticle(Map<String, String> paramMap){
+    public Map deleteArticle(Map<String, String> paramMap) {
 
-        Map map=new LinkedCaseInsensitiveMap();
+        Map map = new LinkedCaseInsensitiveMap();
 
         int a = articleDao.deleteArticleById(paramMap.get("AID"));
 
-        String msg = a > 0? "删除成功" : "删除失败";
+        String msg = a > 0 ? "删除成功" : "删除失败";
         map.put("IS_EXIST", msg);
         return map;
     }
 
-
-
-
-//    MColumn getColumnById(@Param("id") String id);
-//    List<MColumn> getColumnsByTmid(@Param("tmid") String tmid);
-//    int updateMoudleById(MColumn column);
-//    int deleteMouldeById(@Param("id") String id);
-//    int insertColumn(MColumn column);
-
-
+    private boolean isUndefined(String str) {
+        return str == null || "".equals(str.trim()) || str.trim().equals("undefined");
+    }
 
 
 }
